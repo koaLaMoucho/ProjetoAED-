d52277e9a0fbb182fbca88c256440a79281c56bd
DanielCarneiro123 <92723426+DanielCarneiro123@users.noreply.github.com>
2022-11-02 16:42:13 +0000

    Update studentsClassesClass.h

--- studentsClassesClass.h
+++ studentsClassesClass.h
@@ -1,7 +1,6 @@
 #include <string>
 #include <queue>
 #include "classesClass.h"
-
 using namespace std;
 #ifndef UNTITLED_STUDENTSCLASSESCLASS_H
 #define UNTITLED_STUDENTSCLASSESCLASS_H
@@ -16,25 +15,33 @@ public:
 
     static bool strcomp0(string a, string b);
     static bool strcomp1(string a, string b);
-    static void ocupacaoTurmaCout(const string cadeira, string turma, const vector<studentsClassesClass>& arr);
     static int ocupacaoTurma(const string cadeira, string turma, const vector<studentsClassesClass>& arr);
-    static void ocupacaoUcsAno(const vector<studentsClassesClass>& arr, int ano);
+    static void ocupacaoUcsAno(const vector<studentsClassesClass>& arr, char ano);
     static void ocupacaoUc(const vector<studentsClassesClass>& arr, string cadeira);
     static void estudantesTurma(const vector<studentsClassesClass>& arr, string turma);
-    static void estudantesEmUcsAno(const vector<studentsClassesClass>& arr, int ano);
+    static void estudantesEmUcsAno(const vector<studentsClassesClass>& arr, char ano);
     static void todosEstudantes(const vector<studentsClassesClass>& arr);
     static void estudantesUC(const vector<studentsClassesClass>& arr, string cadeira);
     static void turmasUC(const vector<studentsClassesClass>& arr, string uc);
-    static void pedidoAlteracaoHorario(const string nome, const string cadeira, const string novaTurma, vector<studentsClassesClass> &arr, vector<classesClass> &arr2);
+    static void pedidoAlteracaoHorario(const string nome, const string cadeira, const string novaTurma, vector<studentsClassesClass> &arr);
     static bool diferencaDeAlunosTurma(const string cadeira, const vector<studentsClassesClass>& arr);
     static void adicionarEstudante(const string nome, const string cadeira, const string turma, vector<studentsClassesClass>& arr);
     static void removerEstudante(const string nome, const string cadeira, vector<studentsClassesClass>& arr);
     static void alteraçaoVariasTurmas( vector<studentsClassesClass> &arr);
-    static void verificarFinalDoDia(vector<studentsClassesClass> &arr, vector<classesClass> &arr2);
-
+    static void verificarFinalDoDia(vector<studentsClassesClass> &arr);
     static void turmaAluno(const vector<studentsClassesClass> &arr, string nome);
-    static void horarioEstudante(const vector<studentsClassesClass>& arr1, const vector<classesClass>& arr2, string nomeEstudante);
-    static bool verificaSobreposicao(const vector<studentsClassesClass>& arr1, const vector<classesClass>& arr2, string nomeEstudante);
+    static void estudantesEmUcsAno(const vector<studentsClassesClass> &arr, int ano);
+    static void estudantesVariasUcs(const vector<studentsClassesClass> &arr);
+    static set<string> UcsAluno(const vector<studentsClassesClass> &arr, string nome);
+    static void printUcsAluno(const vector<studentsClassesClass> &arr, string nome);
+    static void estudantesNUcs(vector<studentsClassesClass> &arr, int n);
+    static void estudantesNUcsAlfabeto(vector<studentsClassesClass> &arr, int n);
+    static void horarioEstudante(const vector<studentsClassesClass> &arr1, const vector<classesClass> &arr2, string nomeEstudante);
+    static bool verificaSobreposicao(const vector<studentsClassesClass> &arr1, const vector<classesClass> &arr2, string nomeEstudante);
+    static void ocupacaoTurmaCout(const string cadeira, string turma, const vector<studentsClassesClass> &arr);
+    static void pedidoAlteracaoHorario(const string nome, const string cadeira, const string novaTurma, vector<studentsClassesClass> &arr, vector<classesClass> &arr2);
+    static void verificarFinalDoDia(vector<studentsClassesClass> &arr, vector<classesClass> &arr2);
+    static void ocupacaoUcsAno(const vector<studentsClassesClass> &arr, int ano);
 
 };
 
